e2a498089252b02262269138c373e158c9548594
rjgallac <rjgallac@hotmail.com>
2023-09-27 21:47:35 +0100

    fix mapper

--- src/main/java/com/example/demo/model/TransformProperty.java
+++ src/main/java/com/example/demo/model/TransformProperty.java
@@ -1,5 +1,8 @@
 package com.example.demo.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransformProperty {
 
     public Property toProperty(PropertyDto propertyDto) {
